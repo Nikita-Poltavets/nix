9e0689ff90065521d1218baf6b5e01814b2241cc
Taldarim1 <41382312+Taldarim1@users.noreply.github.com>
2020-06-02 17:21:59 +0300

    Update User.java

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -17,6 +17,9 @@ public class User {
 
     @Column(name = "last_name")
     private String lastName;
+    
+    @Column(name = "email")
+    private String email;
 
     public Long getId() {
         return id;
