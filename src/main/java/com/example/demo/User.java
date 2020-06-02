1d1c431f79af077c2b011bfacd421f2c00ec4a43
Taldarim1 <poltavetsnikita@gmail.com>
2020-06-02 18:03:48 +0300

    idk

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -55,4 +55,12 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
